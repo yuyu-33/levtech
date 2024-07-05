0a3a56104d42bed852f6bcd8e3b75554cbc933ff
yuyu-33 <22133237@st.yasuda-u.ac.jp>
2024-07-05 11:34:55 +0900

    8-2 3,4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    public function getPaginateByLimit(int $limit_count = 5)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
